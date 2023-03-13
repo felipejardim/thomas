dfa2a1a1252bd0474d37ff00b3d6f2dbfe526e1a
Felipe Lima Jardim <felipejardim95@gmail.com>
2023-03-12 22:53:21 -0400

    criado mensagem para apoiar usuarios que testarem essa bagunça

--- src/App.js
+++ src/App.js
@@ -23,6 +23,24 @@ function App() {
     <header>
       <p>Thomas</p>
     </header>
+    <div style={{float: "right", width: '400px'}}>
+    <p>
+        Thomas é a fase exploratória do meu tcc, um sistema onde vários usuários conseguiriam enviar músicas de seus serviços preferidos (spotfy, yt, Apple music, etc) para tocar em uma única playlist
+      </p>
+      <p>
+        Este projeto consite em uma serie de testes para entender o comportamento das apis que o sistema final usará. Não terá um layout bem feito ou código bem organizado, é apenas um teste do que funciona e entender o que precisa pra funcionar cada api usada.
+      </p>
+      <p>
+        O comportamento esperado atualmente é:
+      </p>
+      <ul>
+        <li>botão "logar no spotfy" não funcionar</li>
+        <li>ao clicar no botão "mudar" deve trocar o vídeo e tocar automáticamente</li>
+        <li>ao terminar o vídeo o prox deve ser carregado e tocar automáticamente</li>
+        <li>talvez no seu navegador (Firefox provavelmente terá esse comportamento) carregará o primeiro vídeo pausado, porém basta dar play ou clicar em "mudar" que o comportamento descrito acima deve se manter</li>    
+        <li>obviamente os botões em baixo não funcionam :)</li>
+      </ul>
+    </div>
 
     <Spot />
     <Yt src={music} terminou={()=>{setCount(count+1)}}/>
